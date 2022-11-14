1ef2e099b2c8de235b69154946adbc70d933c7ed
יאיר סולטן <yair.sultanv@gmail.com>
2022-11-15 00:57:51 +0200

    First commit

---
+++ Master/app/master_server.py
@@ -0,0 +1,96 @@
+import asyncio
+import time
+import aiohttp
+from typing import List
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+minions = []
+hashes = open('hashes.txt').read().splitlines()
+
+
+class MinionData(BaseModel):
+    """Class that represent minion server with passwords range and hashes to compare."""
+
+    min_range: int
+    max_range: int
+    hashes: List[str]
+
+    def encode(self):
+        """Return object of type dict."""
+
+        return self.__dict__
+
+
+class MinionSub(BaseModel):
+    """Class that represent minion server connection.
+    """
+
+    url: str
+    port: int
+
+
+@app.get("/api/crack-hashes/")
+async def crack_hashes():
+    """Return cracked hashes passwords
+
+    Seperate the work between serveral servers to achieve better performance.
+    """
+
+    start = time.time()
+    minions_data = even_intervals_minions((10 ** 8) - 1, len(minions))
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [post(url, session, data) for url, data in zip(minions, minions_data)]
+        ret = await asyncio.gather(*tasks, return_exceptions=True)
+
+    print(f"Finalized all. Return is a list of len {len(ret)} outputs.\nResult: {ret}")
+    print(f"\n\nTotal time is: {time.time()-start}\n")
+
+    return ret
+
+
+@app.post("/api/subscribe/", status_code=201)
+async def subscribe_minion(data: MinionSub):
+    """Subscribe minion server to the master.
+    """
+
+    minions.append(f"{data.url}:{data.port}")
+    print(minions)
+    print(f"New minion {data.url} has subscribed to master!")
+
+
+async def post(url: str, session: aiohttp.ClientSession, body: MinionData):
+    """Post a single request
+
+    Return json with Host, Status and Cracked_hashes dict.
+    """
+
+    try:
+        async with session.post(url=url + "/api/crack-hashes-in-range/", json=body.encode()) as response:
+            resp = await response.json()
+            print(f"Successfully got url {url} with res: {resp}.")
+
+            return resp
+
+    except Exception as e:
+        print(f"Unable to get url {url} due to {e}.")
+        
+        return {"Host": url, "Status": "Failed", "Exception": e}
+
+
+def even_intervals_minions(value: int, n: int) -> List[MinionData]:
+    """Generate MinionData objects and initialize them with even interval ranges.
+    """
+
+    minions = []
+    bin_size = (value + 1) // n
+
+    for x in range (n):
+        minions.append(MinionData(min_range=x * bin_size, max_range=(x + 1) * bin_size - 1, hashes=hashes))
+
+    # In case we have uneven intervals we add the reminder to the last range
+    minions[n - 1].max_range = 99999999
+        
+    return minions
---
+++ Minion/app/minion_server.py
@@ -0,0 +1,97 @@
+import hashlib
+import os
+import aiohttp
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from time import sleep
+
+app = FastAPI()
+
+
+class MinionData(BaseModel):
+    """Class that represent minion server with passwords range and hashes to compare."""
+
+    min_range: int
+    max_range: int
+    hashes: List[str]
+
+
+class MinionSub(BaseModel):
+    """Class that represent minion server connection.
+    """
+
+    url: str
+    port: int
+
+    def encode(self):
+        """Return object of type dict."""
+
+        return self.__dict__
+    
+
+@app.on_event("startup")
+async def startup_event():
+    """Create connection to master server on startup.
+    """
+    async with aiohttp.ClientSession() as session:
+        await subscribe_to_master(f'http://{os.getenv("MASTER_HOST")}:8000', session)
+
+
+@app.post("/api/crack-hashes-in-range/")
+async def crack_hashes(data: MinionData):
+    """Post method receving MinionData object
+
+    Loops over range of passwords calculate their MD5 hashes and compare with the given hashes.
+    """
+
+    try:
+        # Create hash dict to optimize hash comparing to average O(1)
+        hash_dict = dict.fromkeys(data.hashes)
+
+        # Result dictionary where key is the hash and value is the password
+        result_dict = {}
+        minion_url = f'http://{os.getenv("HOST_NAME")}:{int(os.getenv("APP_PORT"))}'
+
+        for i in range(data.min_range, data.max_range + 1):
+            phone_number = get_phone_number(str(i))
+            hash = hashlib.md5(phone_number.encode()).hexdigest()
+
+            if hash in hash_dict:
+                print(f"Found match! {phone_number} \n")
+                result_dict[hash] = phone_number
+            
+        return {"Host": minion_url,
+                "Status": "Success",
+                "Cracked_passwords": result_dict}
+
+    except Exception as e:
+        result_dict["Status"] = "Failed"
+
+        return {"Host": minion_url,
+                "Status": "Failed",
+                "Exception": e}
+
+
+def get_phone_number(num: str) -> str:
+    """Returns given number as phone number - add zeros to prefix when needed."""
+    
+    prefix = "05" + "0" * (8 - len(num))
+    
+    return prefix + num
+
+
+async def subscribe_to_master(master_url: str, session: aiohttp.ClientSession):
+    try:
+        minion_sub = MinionSub(url=f'http://{os.getenv("HOST_NAME")}', port=int(os.getenv("APP_PORT")))
+        url = master_url + "/api/subscribe/"
+
+        async with session.post(url=url, json=minion_sub.encode()) as response:
+            resp = await response.json()
+            print(f"Successfully subscribed {minion_sub.url} to Master.")
+
+            return resp
+
+    except Exception as e:
+        print(f"Unable to subscribe url {minion_sub.url} due to {e}.")
+
